662529abb73b6991286b37d9306c6755b9b1583d
Felipe Rezende <feliperezende.srz@gmail.com>
2020-02-14 21:38:22 -0300

    Conectando com a api de filmes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("HELLO");
+const baseUrl = require('./src/base-url');
+const request = require('request');
+
+function getAllMovies() {
+    request(`${baseUrl.BASE_URL}discover/movie?api_key=${baseUrl.API_KEY}`, { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+
+      const data = body.results.map(b => {
+          return {
+              title: b.title,
+              overview: b.overview
+          }
+      });
+
+      insertOnHTML(data);
+    });
+}
+
+getAllMovies();
---
+++ src/base-url.js
@@ -0,0 +1,5 @@
+const API_KEY = "05db07e27024bc56a1e3aa80f74fc6bd";
+const LANGUAGE = "pt-BR";
+const BASE_URL = "https://api.themoviedb.org/3/";
+
+module.exports = {API_KEY, LANGUAGE, BASE_URL} 
