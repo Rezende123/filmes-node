ff767fd2560924a05e9cc7f2d406c03ada22b219
Felipe Rezende <feliperezende.srz@gmail.com>
2020-02-14 21:58:10 -0300

    Gerando arquivo html com os dados da api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const baseUrl = require('./src/base-url');
 const request = require('request');
+const ejs = require('ejs');
+const fs = require('fs');
+const htmlFile = '/home/felipe/Documentos/GIT/filmes-node/src/view.html';
 
 function getAllMovies() {
     request(`${baseUrl.BASE_URL}discover/movie?api_key=${baseUrl.API_KEY}`, { json: true }, (err, res, body) => {
@@ -13,6 +16,18 @@ function getAllMovies() {
       });
 
       insertOnHTML(data);
+    });
+}
+
+function insertOnHTML(data) {
+    ejs.renderFile('./src/layout.ejs', {data}, function(err, html){
+        if (html) {
+            var stream = fs.createWriteStream(htmlFile);
+
+            stream.once('open', function(fd) {
+                stream.end(html);
+            });
+        }
     });
 }
 
